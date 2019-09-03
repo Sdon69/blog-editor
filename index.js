bcded245cb64007a54c962ef957932fb65c7755f
Sdon69 <suyash.99.doneria@gmail.com>
2019-09-04 02:51:09 +0530

    Added Charges Support

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const request = require('request-promise');
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'read_products,read_themes, write_themes,read_content, write_content';
-const forwardingAddress = "https://nameless-hollows-55097.herokuapp.com"; // Replace this with your HTTPS Forwarding address
+// const forwardingAddress = "https://nameless-hollows-55097.herokuapp.com"; // Replace this with your HTTPS Forwarding address
+const forwardingAddress = "https://26fcb803.ngrok.io";
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
@@ -62,9 +63,12 @@ app.post('/save', function (req, res) {
 })
 
 
-app.listen(port, () => {
-  console.log('Example app listening on port' + port);
-});
+app.listen(3000, () => {
+  console.log('Example app listening on port' + 3000);
+
+  });
+  
+
 
 app.get('/shopify', (req, res) => {
 	
@@ -87,6 +91,160 @@ app.get('/shopify', (req, res) => {
   }
 });
 app.get('/shopify/callback', (req, res) => {
+	console.log("ins1");
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+  
+  
+
+  if (shop && hmac && code) {
+	  console.log("ins2");
+    // DONE: Validate request is from Shopify
+    const map = Object.assign({}, req.query);
+    delete map['signature'];
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const providedHmac = Buffer.from(hmac, 'utf-8');
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+        'utf-8'
+      );
+    let hashEquals = false;
+
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    } catch (e) {
+      hashEquals = false;
+    };
+
+    if (!hashEquals) {
+      return res.status(400).send('HMAC validation failed');
+    }
+
+    // DONE: Exchange temporary code for a permanent access token
+    const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+    const accessTokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code,
+    };
+
+	
+	
+	
+	
+	
+
+    request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+    .then((accessTokenResponse) => {
+      const accessToken = accessTokenResponse.access_token;
+      // DONE: Use access token to make API call to 'shop' endpoint
+	  
+	  const text = '{"asset":[' +
+'{"firstName":"John","lastName":"Doe" },' +
+'{"firstName":"Anna","lastName":"Smith" },' +
+'{"firstName":"Peter","lastName":"Jones" }]}';
+
+
+         const shopRequestUrl = 'https://' + shop + '/admin/api/2019-07/recurring_application_charges.json?recurring_application_charge[name]=Easy Blog Creator&recurring_application_charge[price]=0.99&recurring_application_charge[return_url]='+forwardingAddress +'&recurring_application_charge[trial_days]=14';
+		 const shopRequestUrl2 = 'https://' + shop + '/admin/api/2019-04/themes/41456828451/asset.json?asset[key]=assets/getAsin8.txt';
+		 const shopRequestUrl3 = 'https://' + shop + '/admin/api/2019-07/recurring_application_charges.json';
+	     const shopRequestUrl4 = 'https://' + shop + '/admin/api/2019-04/themes/41456828451/assets.json?asset[key]=assets/getAsin8.txt&theme_id=41456828451&fields=public_url';
+    
+      const shopRequestHeaders = {
+		   'Content-Type': 'application/json',
+        'X-Shopify-Access-Token': accessToken
+		  
+      };
+	  const putContent = {
+		  'Content-Type': 'application/json',
+              'key': "assets/getAsin.txt",
+           }
+
+   
+	  request.get(shopRequestUrl3, { headers: shopRequestHeaders })
+     .then((shopResponse) => {
+    
+	appInstalled = "";
+	    shopResponseObj = JSON.parse(shopResponse);
+	
+	
+		if(shopResponse.includes("accepted"))
+		{
+			
+			
+			 console.log(appInstalled);
+			fs.readFile('./public/test.html', null, function(error, data)
+	  {
+		  if(error)
+		  {	
+		
+		  res.write('File Not Found')
+		  }	else
+		  {
+			
+			   
+			   res.end(data);
+			   
+			
+		  }
+		 
+	  });
+		}
+		else
+		{
+			
+			console.log(shop);
+			const saveRedirectUri = forwardingAddress + "/shopify-request?shop="+shop;
+			res.redirect(saveRedirectUri);
+		}
+		
+	
+	
+	
+	  })
+      })
+	  
+	   } else {
+    res.status(400).send('Required parameters missing');
+  }
+});
+
+
+	  
+	  app.get('/shopify-request', (req, res) => {
+  	console.log("shopify-request initiated");
+
+const shop = req.query.shop;
+  if (shop) {
+	  
+    const state = nonce();
+	console.log(forwardingAddress);
+    const redirectUri = forwardingAddress + '/shopify/callback-request';
+    const installUrl = 'https://' + shop +
+      '/admin/oauth/authorize?client_id=' + apiKey +
+      '&scope=' + scopes +
+      '&state=' + state +
+      '&redirect_uri=' + redirectUri;
+
+    res.cookie('state', state);
+    res.redirect(installUrl);
+  } else {
+    return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+  }
+});
+
+app.get('/shopify/callback-request', (req, res) => {
+	console.log("callback-request-initiated");
+	
+ 
   const { shop, hmac, code, state } = req.query;
   const stateCookie = cookie.parse(req.headers.cookie).state;
 
@@ -132,76 +290,50 @@ app.get('/shopify/callback', (req, res) => {
 
     request.post(accessTokenRequestUrl, { json: accessTokenPayload })
     .then((accessTokenResponse) => {
+		
+	
+		
+		
       const accessToken = accessTokenResponse.access_token;
       // DONE: Use access token to make API call to 'shop' endpoint
+	
+   
 	  
-	  const text = '{"asset":[' +
-'{"firstName":"John","lastName":"Doe" },' +
-'{"firstName":"Anna","lastName":"Smith" },' +
-'{"firstName":"Peter","lastName":"Jones" }]}';
-
-
-      const shopRequestUrl = 'https://' + shop + '/admin/api/2019-04/themes/41456828451/assets.json?asset[key]=assets/getAsin6.txt&asset[value]=';
-	  const shopRequestUrl2 = 'https://' + shop + '/admin/api/2019-04/themes/41456828451/asset.json?asset[key]=assets/getAsin8.txt';
-	  const shopRequestUrl3 = 'https://' + shop + '/admin/api/2019-04/blogs/25510117411/articles.json';
-	     const shopRequestUrl4 = 'https://' + shop + '/admin/api/2019-04/themes/41456828451/assets.json?asset[key]=assets/getAsin8.txt&theme_id=41456828451&fields=public_url';
-    
       const shopRequestHeaders = {
 		   'Content-Type': 'application/json',
         'X-Shopify-Access-Token': accessToken
 		  
       };
-	  const putContent = {
-		  'Content-Type': 'application/json',
-              'key': "assets/getAsin.txt",
-           }
+	 
 
-   
+	   const shopRequestUrl = 'https://' + shop + '/admin/api/2019-07/recurring_application_charges.json?recurring_application_charge[name]=Easy Blog Creator&recurring_application_charge[price]=0.99&recurring_application_charge[return_url]='+forwardingAddress +'&recurring_application_charge[trial_days]=14';
 	
-	
-	
-     request.get(shopRequestUrl4, { headers: shopRequestHeaders} )
-      .then((shopResponse) => {
-		  
-        
-		fs.readFile('./public/test.html', null, function(error, data)
-	{
-		if(error)
-		{	
-		
-		res.write('File Not Found')
-		}	else
-		{
-			
-			
-			res.write(data);
-			res.end("");
-		}
-		 
-	});
-		
-      }) .catch((error) => {
-		  
-       fs.readFile('./public/test.html', null, function(error, data)
-	{
-		if(error)
-		{	
-		
-		res.write('File Not Found')
-		}	else
-		{
-			
-			
-			res.write(data);
-			res.end("");
-		}
-		 
-	});
-      });
 	  
-
-    
-    
+		request.post(shopRequestUrl, { headers: shopRequestHeaders} )
+       .then((shopResponse) => {
+		   
+		
+		 
+		 const  shopResponseString   = shopResponse;
+		  
+		  const shopResponseObj = JSON.parse(shopResponseString);
+		 const paymentConfirmationUrl = shopResponseObj.recurring_application_charge.confirmation_url;
+		 
+      
+	
+			
+			  res.redirect(paymentConfirmationUrl);
+			
+		  
+	
+		
+       }) .catch((error) => {
+		  
+		  var errorString =  JSON.stringify(error);
+		   res.write(errorString);
+		   res.end("");
+        
+       }); 
 	
      
 	
@@ -214,3 +346,22 @@ app.get('/shopify/callback', (req, res) => {
     res.status(400).send('Required parameters missing');
   }
 });
+     
+	
+	
+     
+	  
+
+	  
+
+    
+    
+	
+     
+	
+	
+	 
+	   
+    
+
+ 
